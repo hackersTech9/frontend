8600238f4839393a87e031e56034883e99123971
Ailin Pytel <ailin.o.pytel@gmail.com>
2022-03-17 18:41:09 -0300

    se desarrolla componente de contenido

--- src/Components/Stripe/StripeComponent.jsx
+++ src/Components/Stripe/StripeComponent.jsx
@@ -1,5 +1,46 @@
-export const Stripe = () => {
+import { Stack, Image, Text } from '@chakra-ui/react'
+import { useNavigate } from '../../../node_modules/react-router-dom/index'
+
+
+
+export const Stripe = ({sectionTitle, billboard }) => {
+
+  const navigate = useNavigate();
+
+  const handleOnClick = (url) => {
+    // navigate('/play', { state: {contentSrc: url}});
+    console.log(url)
+  }
+  
   return(
-    <div>esto es stripe container</div>
+    <>
+    <div p={6}>{sectionTitle}</div>
+    <Stack  direction= "row" p={10}>
+      {billboard.map((v, i) => {
+        
+        return(
+          <div  key={i} >
+          <Text >
+          {v.name}
+          </Text>
+          <div onClick={() => {
+            handleOnClick(v.url)
+          }}>
+          <Image 
+              boxSize='100%'  
+              objectFit='cover'
+          src={v.thumbnail ? v.thumbnail : "" }
+          alt={v.name}
+          />
+          </div>
+          </div>
+        )
+      })
+    }
+
+    </Stack>
+    </>
   )
 } 
+
+
