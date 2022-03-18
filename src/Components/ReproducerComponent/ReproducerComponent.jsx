c5cb007dd46f493f9f75656502058a761c1fd024
Janegro09 <javier.sosa099@gmail.com>
2022-03-18 10:36:49 -0300

    sumamos social media

--- src/Components/ReproducerComponent/ReproducerComponent.jsx
+++ src/Components/ReproducerComponent/ReproducerComponent.jsx
@@ -2,18 +2,29 @@ import React, { useEffect, useState } from "react";
 
 import { useLocation, useNavigate } from "react-router-dom/";
 
-import { Container, Stack, Button, Text, Image } from '@chakra-ui/react';
+import { Container, Stack, Button, Text,   Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  HStack
+ } from '@chakra-ui/react';
 
 import { BsFillShareFill } from 'react-icons/bs'
+import { FaFacebook, FaTwitter, FaWhatsapp } from 'react-icons/fa'
+
+
 
 export const Reproducer = () => {
   
   const navigate = useNavigate();
   const { state } = useLocation();
   const [content, setContent] = useState(null);
-
+  const [isOpen, setIsOpen] = useState(false)
+  // const [onClose, setOnClose] = useState(false)
   useEffect(() => {
     if(!state?.contentSrc || !state?.contentTitle) return navigate('/notfound');
+
+    
 
     const { contentSrc, contentTitle } = state;
     const contentToRender = {
@@ -26,6 +37,13 @@ export const Reproducer = () => {
 
   const handleNav = () => {
     navigate("/home");
+  }
+
+  const encenderModal = () => {
+    setIsOpen(true)
+  }
+  const cerrarModal = () => {
+    setIsOpen(false)
   }
 
   return(
@@ -48,14 +66,52 @@ export const Reproducer = () => {
         <Stack direction='row' spacing={4}>
           <Button width="100%" onClick={handleNav} variant='btnMain'>Volver</Button>
           <Button 
-              variant='btnMain'
-              leftIcon={
-                <BsFillShareFill/>
+              onClick={
+                encenderModal
               }
+              variant='btnMain'
+              borderRadius="2000px"
+              width="48px"
               >
+                <BsFillShareFill/>
           </Button>
         </Stack>
       </Stack>
+      <Modal isOpen={isOpen} onClose={cerrarModal} isCentered>
+        <ModalOverlay />
+        <ModalContent 
+          // border="20px"
+        >
+          <ModalBody>
+            <HStack>
+              <Button 
+                colorScheme='facebook' 
+                leftIcon={<FaFacebook />}
+                size="lg"
+                >
+                Facebook
+              </Button>
+              <br></br>
+              <Button 
+                colorScheme='twitter' 
+                leftIcon={<FaTwitter />}
+                size="lg"
+                >
+                Twitter
+              </Button>
+                  <br></br>
+              <Button 
+                colorScheme='whatsapp' 
+                leftIcon={<FaWhatsapp />}
+                size="lg"
+                >
+              whatsapp
+              </Button>
+                  <br></br>
+            </HStack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Container>  
   )
 };
